17fbc4aa74ca1abdb64c656f6bd1ae6c80652eb8
Cemonix <gittlermilan@seznam.cz>
2025-05-25 15:12:02 +0200

    Add Annotation record to represent annotations in the domain model

---
+++ server/Models/Domain/Annotation.cs
@@ -0,0 +1,34 @@
+using server.Models.Domain.Enums;
+
+namespace server.Models.Domain;
+
+public record class Annotation
+{
+    public long AnnotationId { get; init; }
+    public AnnotationType AnnotationType { get; init; }
+    public string Data { get; init; } = string.Empty;
+    public bool IsPrediction { get; init; }
+    public double? ConfidenceScore { get; init; }
+    public bool IsGroundTruth { get; init; }
+    public int Version { get; init; }
+    public string? Notes { get; init; }
+
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+
+    // Foreign Keys
+    public int TaskId { get; init; }
+    public int AssetId { get; init; }
+    public int LabelId { get; init; }
+    public string AnnotatorUserId { get; init; } = string.Empty;
+    public long? ParentAnnotationId { get; init; }
+
+    // Navigation Properties
+    public virtual Task Task { get; init; } = null!;
+    public virtual Asset Asset { get; init; } = null!;
+    public virtual Label Label { get; init; } = null!;
+    public virtual ApplicationUser AnnotatorUser { get; init; } = null!;
+    public virtual Annotation? ParentAnnotation { get; init; }
+    public virtual ICollection<Annotation> ChildAnnotations { get; init; } = [];
+    public virtual ICollection<Issue> Issues { get; init; } = [];
+}
