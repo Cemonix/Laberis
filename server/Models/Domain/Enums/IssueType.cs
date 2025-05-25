64e4755d42a370730cbf82e898cd44f59653e576
Cemonix <gittlermilan@seznam.cz>
2025-05-25 15:11:31 +0200

    Add IssueType enum to represent various issue types in the domain model

---
+++ server/Models/Domain/Enums/IssueType.cs
@@ -0,0 +1,11 @@
+namespace server.Models.Domain.Enums;
+
+public enum IssueType
+{
+    INCORRECT_ANNOTATION,
+    MISSING_ANNOTATION,
+    AMBIGUOUS_TASK,
+    ASSET_QUALITY_ISSUE,
+    GUIDELINE_INQUIRY,
+    OTHER
+}
