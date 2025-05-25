6fb0893b1761df96787800b797c17c091022d6a6
Cemonix <gittlermilan@seznam.cz>
2025-05-25 13:11:55 +0200

    Add ProjectType enum to define various project types

---
+++ server/Models/Domain/Enums/ProjectType.cs
@@ -0,0 +1,11 @@
+namespace server.Models.Domain.Enums;
+
+public enum ProjectType
+{
+    IMAGE_CLASSIFICATION,
+    OBJECT_DETECTION,
+    IMAGE_SEGMENTATION,
+    VIDEO_ANNOTATION,
+    TEXT_ANNOTATION,
+    OTHER
+}
