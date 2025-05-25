8776e5ccfdd504f71e8490899074ea9e983514d4
Cemonix <gittlermilan@seznam.cz>
2025-05-25 15:14:44 +0200

    Add TaskEventConfiguration to define entity configuration and relationships for TaskEvent

---
+++ server/Data/Configurations/TaskEventConfiguration.cs
@@ -0,0 +1,61 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using server.Models.Domain;
+
+namespace server.Data.Configurations;
+
+public class TaskEventConfiguration : IEntityTypeConfiguration<TaskEvent>
+{
+    public void Configure(EntityTypeBuilder<TaskEvent> entity)
+    {
+        entity.ToTable("task_events");
+
+        entity.HasKey(te => te.EventId);
+        entity.Property(te => te.EventId).HasColumnName("event_id").ValueGeneratedOnAdd();
+
+        entity.Property(te => te.EventType)
+            .HasColumnName("event_type")
+            .IsRequired()
+            .HasColumnType("task_event_type_enum");
+
+        entity.Property(te => te.Details)
+            .HasColumnName("details")
+            .HasColumnType("jsonb")
+            .IsRequired(false);
+
+        entity.Property(te => te.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired()
+            .HasDefaultValueSql("CURRENT_TIMESTAMP")
+            .ValueGeneratedOnAdd();
+
+        entity.Property(te => te.TaskId).HasColumnName("task_id");
+        entity.Property(te => te.UserId).HasColumnName("user_id").IsRequired(false);
+        entity.Property(te => te.FromWorkflowStageId).HasColumnName("from_workflow_stage_id").IsRequired(false);
+        entity.Property(te => te.ToWorkflowStageId).HasColumnName("to_workflow_stage_id").IsRequired(false);
+
+        // Relationships
+        entity.HasOne(te => te.Task)
+            .WithMany(t => t.TaskEvents)
+            .HasForeignKey(te => te.TaskId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        entity.HasOne(te => te.User)
+            .WithMany()
+            .HasForeignKey(te => te.UserId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        entity.HasOne(te => te.FromWorkflowStage)
+            .WithMany()
+            .HasForeignKey(te => te.FromWorkflowStageId)
+            .OnDelete(DeleteBehavior.SetNull)
+            .IsRequired(false);
+
+        entity.HasOne(te => te.ToWorkflowStage)
+            .WithMany()
+            .HasForeignKey(te => te.ToWorkflowStageId)
+            .OnDelete(DeleteBehavior.SetNull)
+            .IsRequired(false);
+    }
+}
