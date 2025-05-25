ebf1df488e32fbbdeb711c2745b78608acf88d07
Cemonix <gittlermilan@seznam.cz>
2025-05-25 15:11:47 +0200

    Add Workflow record to represent workflows in the domain model

---
+++ server/Models/Domain/Workflow.cs
@@ -0,0 +1,24 @@
+namespace server.Models.Domain;
+
+public record class Workflow
+{
+    public int WorkflowId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? Description { get; init; }
+    public bool IsDefault { get; init; }
+
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+
+    // Foreign Key to Project
+    public int ProjectId { get; init; }
+
+    // Navigation Properties
+    public virtual Project Project { get; init; } = null!;
+
+    // A workflow consists of multiple stages
+    public virtual ICollection<WorkflowStage> WorkflowStages { get; init; } = [];
+
+    // A workflow definition is used by many tasks
+    public virtual ICollection<Task> Tasks { get; init; } = [];
+}
