eba009f92aca1ba04d949092913a5c9732294f33
Cemonix <gittlermilan@seznam.cz>
2025-05-25 13:12:30 +0200

    Add Label record to represent labels in the project

---
+++ server/Models/Domain/Label.cs
@@ -0,0 +1,20 @@
+namespace server.Models.Domain;
+
+public record class Label
+{
+    public int LabelId { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? Color { get; init; }
+    public string? Description { get; init; }
+    public string? Metadata { get; init; }
+
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+
+    // Foreign Key to LabelScheme
+    public int LabelSchemeId { get; init; }
+
+    // Navigation Properties
+    public virtual LabelScheme LabelScheme { get; init; } = null!;
+    public virtual ICollection<Annotation> Annotations { get; init; } = [];
+}
