86db05673ebd64149dcec39103f2287c3993a835
Cemonix <gittlermilan@seznam.cz>
2025-05-26 13:58:48 +0200

    Add IGenericRepository interface for generic repository pattern

---
+++ server/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq.Expressions;
+
+namespace server.Repositories;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(object id);
+    Task<IEnumerable<T>> GetAllAsync(
+        string? filterOn = null, string? filterQuery = null, string? sortBy = null,
+        bool isAscending = true, int pageNumber = 1, int pageSize = 25
+    );
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<int> SaveChangesAsync();
+
+}
