1265106546d87eb33470ac521c354002f92c6020
Cemonix <gittlermilan@seznam.cz>
2025-06-01 11:26:50 +0200

    Add Footer component with template, script, and styles

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer">
+        <p>&copy; Laberis</p>
+    </footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.footer {
+    text-align: center;
+    padding: 0.5rem;
+    background-color: #f8f9fa;
+    border-top: 1px solid #e7e7e7;
+    width: 100%;
+}
+</style>
