6c7c1ca3218234ba74b467ea1c301ef94f6a6fb1
Cemonix <gittlermilan@seznam.cz>
2025-06-01 11:27:03 +0200

    Refactor router configuration to include HomePage and AnnotationWorkspace components

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,41 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-
+import HomePage from '../views/HomePage.vue';
+import AnnotationWorkspace from '../views/AnnotationWorkspace.vue';
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+import WorkspaceLayout from '../layouts/WorkspaceLayout.vue';
 
 const routes: Array<RouteRecordRaw> = [
-    { path: '/', redirect: '/home' },
-    // {
-    //   path: '/home',
-    //   name: 'Home',
-    //   component: () => import('../views/HomePage.vue')
-    // },
     {
-        path: '/labelling',
-        name: 'LabellingPage',
-        component: () => import('../views/LabellingPage.vue'),
+        path: '/',
+        redirect: '/home',
     },
+    {
+        path: '/home',
+        name: 'Home',
+        component: HomePage,
+        meta: {
+            layout: DefaultLayout,
+        }
+    },
+    {
+        
+        path: '/workspace/project/:projectId/asset/:assetId',
+        name: 'AnnotationWorkspace',
+        component: AnnotationWorkspace,
+        props: true,
+        meta: {
+            layout: WorkspaceLayout,
+        }
+    },
+    {
+        path: '/projects',
+        name: 'ProjectList',
+        // TODO: component: () => import('../views/ProjectListPage.vue'), // Create this later
+        component: { template: '<div>Project List Page - Uses Default Layout</div>' }, // Placeholder
+        meta: {
+            layout: DefaultLayout,
+        }
+    }
 ];
 
 const router = createRouter({
