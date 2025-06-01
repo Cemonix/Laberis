dd36ea91c9c4a90f2572ef10c60d8d5c9514a66e
Cemonix <gittlermilan@seznam.cz>
2025-06-01 11:26:38 +0200

    Add HomePage component with initial template and styles

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="home-container">
+		<h1>Welcome to Laberis</h1>
+	</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.home-container {
+	display: flex;
+	justify-content: center;
+	align-items: start;
+	background-color: #f0f0f0;
+	width: 100%;
+	padding: 1rem;
+}
+
+h1 {
+	text-align: center;
+	color: #333;
+}
+</style>
