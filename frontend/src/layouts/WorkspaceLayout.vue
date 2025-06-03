6bd9adc0800816a1207548ba076752bf4e3a24f0
Cemonix <gittlermilan@seznam.cz>
2025-06-03 22:42:27 +0200

    Refactor whitespace and formatting in WorkspaceLayout.vue for improved readability

--- frontend/src/layouts/WorkspaceLayout.vue
+++ frontend/src/layouts/WorkspaceLayout.vue
@@ -1,22 +1,21 @@
 <template>
-  <div class="workspace-layout">
-    <slot /> </div>
+    <div class="workspace-layout">
+        <slot />
+    </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
 @use "@/styles/variables.scss" as vars;
 
 .workspace-layout {
-  height: 100vh;
-  width: 100vw;
-  overflow: hidden;
-  background-color: vars.$workspace-layout-bg;
-  color: vars.$workspace-layout-text;
-  display: flex;
-  flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    background-color: vars.$workspace-layout-bg;
+    color: vars.$workspace-layout-text;
+    display: flex;
+    flex-direction: column;
 }
 </style>
