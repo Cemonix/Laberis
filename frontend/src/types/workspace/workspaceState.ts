71a6f5a476b4f09615ce5885ef28215ced0d0591
Cemonix <gittlermilan@seznam.cz>
2025-06-05 20:50:48 +0200

    Add annotations and label scheme properties to WorkspaceState interface

--- frontend/src/types/workspace/workspaceState.ts
+++ frontend/src/types/workspace/workspaceState.ts
@@ -2,6 +2,8 @@ import type { ImageDimensions } from "@/types/image/imageDimensions";
 import type { Timer } from "@/utils/timer";
 import type { Point } from "@/types/common/point";
 import type { Tool, ToolName } from "@/types/workspace/tools";
+import type { Annotation } from '@/types/workspace/annotation';
+import type { LabelScheme } from '@/types/workspace/labelScheme';
 
 export interface WorkspaceState {
     currentProjectId: string | null;
@@ -16,4 +18,8 @@ export interface WorkspaceState {
     zoomLevel: number;
     activeTool: ToolName;
     availableTools: Tool[];
+    annotations: Annotation[];
+    currentLabelId: number | null;
+    currentLabelScheme: LabelScheme | null;
+    currentTaskId: number | null;
 }
