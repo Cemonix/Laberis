3d9e96d8e5ff7bfaa2407daa0fb67db44ab6db41
Cemonix <gittlermilan@seznam.cz>
2025-06-07 17:39:30 +0200

    Add LabelChip component for displaying label information with customizable styles

---
+++ frontend/src/components/labels/LabelChip.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="label-chip" :style="{ backgroundColor: chipColor, color: textColor }">
+        {{ label.name }}
+    </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { Label } from '@/types/label/label';
+
+const props = defineProps<{
+    label: Label;
+}>();
+
+const chipColor = computed(() => {
+    // TODO: This is a simple way to adjust color, but for true WCAG contrast, a more robust library might be needed.
+    return props.label.color + '33';
+});
+
+const textColor = computed(() => props.label.color);
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/variables.scss" as vars;
+
+.label-chip {
+    display: inline-block;
+    padding: 0.25em 0.75em;
+    border-radius: 1em;
+    font-size: vars.$font_size_small;
+    font-weight: vars.$font_weight_heading;
+    white-space: nowrap;
+}
+</style>
