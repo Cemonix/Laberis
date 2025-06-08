17389798370e74d21fdeff24b9257dd4a6b7ad0b
Cemonix <gittlermilan@seznam.cz>
2025-06-08 15:35:27 +0200

    add DataExplorerLayout component with initial template and styles

---
+++ frontend/src/layouts/DataExplorerLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="data-explorer-layout">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/variables" as vars;
+
+.data-explorer-layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+    background-color: vars.$theme-background;
+    color: vars.$theme-text;
+}
+</style>
