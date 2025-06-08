0f7c5af7c9cf8e771ef8352cfb90241205e683bb
Cemonix <gittlermilan@seznam.cz>
2025-06-08 15:35:17 +0200

    add AlertModal component and integrate alert handling in DefaultLayout

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -7,12 +7,23 @@
             <slot />
         </main>
         <Footer />
+
+        <AlertModal
+            :is-open="isAlertOpen"
+            :title="alertTitle"
+            :message="alertMessage"
+            @confirm="handleConfirm"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/layouts/Navbar.vue';
 import Footer from '@/layouts/Footer.vue';
+import AlertModal from '@/components/common/modals/AlertModal.vue';
+import { useAlert } from '@/composables/useAlert';
+
+const { isAlertOpen, alertTitle, alertMessage, handleConfirm } = useAlert();
 </script>
 
 <style lang="scss" scoped>
