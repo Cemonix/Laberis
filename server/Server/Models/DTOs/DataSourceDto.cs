704c188a6e48434e4c33439e84c72b9438dea5d0
Cemonix <gittlermilan@seznam.cz>
2025-06-09 20:47:22 +0200

    add DataSourceDto record to represent data source information

---
+++ server/Server/Models/DTOs/DataSourceDto.cs
@@ -0,0 +1,15 @@
+using server.Models.Domain.Enums;
+
+namespace server.Models.DTOs;
+
+public record class DataSourceDto
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string? Description { get; init; }
+    public DataSourceType SourceType { get; init; }
+    public DataSourceStatus Status { get; init; }
+    public bool IsDefault { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public int ProjectId { get; init; }
+}
