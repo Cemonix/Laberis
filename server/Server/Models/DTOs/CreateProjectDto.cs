32187a89114bc8d312b3ad4535da3453edf39ce3
Cemonix <gittlermilan@seznam.cz>
2025-06-09 20:47:26 +0200

    add CreateProjectDto record to define project creation data structure

---
+++ server/Server/Models/DTOs/CreateProjectDto.cs
@@ -0,0 +1,17 @@
+using server.Models.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.DTOs;
+
+public record class CreateProjectDto
+{
+    [Required(ErrorMessage = "Project name is required.")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Project name must be between 3 and 100 characters.")]
+    public string Name { get; init; } = string.Empty;
+
+    public string? Description { get; init; }
+
+    [Required(ErrorMessage = "Project type is required.")]
+    [EnumDataType(typeof(ProjectType), ErrorMessage = "Invalid project type specified.")]
+    public ProjectType ProjectType { get; init; }
+}
