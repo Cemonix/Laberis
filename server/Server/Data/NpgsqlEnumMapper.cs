8cef089b6884094efb77899b2fd444c70e8041d2
Cemonix <gittlermilan@seznam.cz>
2025-06-10 20:06:29 +0200

    refactor NpgsqlEnumMapper to use NpgsqlDbContextOptionsBuilder for enum mapping

--- server/Server/Data/NpgsqlEnumMapper.cs
+++ server/Server/Data/NpgsqlEnumMapper.cs
@@ -1,22 +1,27 @@
 using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure;
 using server.Models.Domain.Enums;
 
 namespace server.Data;
 
 public static class NpgsqlEnumMapper
 {
-    public static void MapEnums(NpgsqlDataSourceBuilder dataSourceBuilder)
+    /// <summary>
+    /// Configures Npgsql to map all C# enums to their corresponding PostgreSQL enum types.
+    /// </summary>
+    /// <param name="npgsqlOptions">The Npgsql options builder provided by UseNpgsql().</param>
+    public static void ConfigureEnums(NpgsqlDbContextOptionsBuilder npgsqlOptions)
     {
-        dataSourceBuilder.MapEnum<ProjectType>("project_type_enum");
-        dataSourceBuilder.MapEnum<ProjectStatus>("project_status_enum");
-        dataSourceBuilder.MapEnum<ProjectRole>("project_role_enum");
-        dataSourceBuilder.MapEnum<DataSourceType>("data_source_type_enum");
-        dataSourceBuilder.MapEnum<DataSourceStatus>("data_source_status_enum");
-        dataSourceBuilder.MapEnum<WorkflowStageType>("workflow_stage_type_enum");
-        dataSourceBuilder.MapEnum<TaskEventType>("task_event_type_enum");
-        dataSourceBuilder.MapEnum<IssueStatus>("issue_status_enum");
-        dataSourceBuilder.MapEnum<IssueType>("issue_type_enum");
-        dataSourceBuilder.MapEnum<AnnotationType>("annotation_type_enum");
-        dataSourceBuilder.MapEnum<AssetStatus>("asset_status_enum");
+        npgsqlOptions.MapEnum<ProjectType>("project_type_enum");
+        npgsqlOptions.MapEnum<ProjectStatus>("project_status_enum");
+        npgsqlOptions.MapEnum<ProjectRole>("project_role_enum");
+        npgsqlOptions.MapEnum<DataSourceType>("data_source_type_enum");
+        npgsqlOptions.MapEnum<DataSourceStatus>("data_source_status_enum");
+        npgsqlOptions.MapEnum<WorkflowStageType>("workflow_stage_type_enum");
+        npgsqlOptions.MapEnum<TaskEventType>("task_event_type_enum");
+        npgsqlOptions.MapEnum<IssueStatus>("issue_status_enum");
+        npgsqlOptions.MapEnum<IssueType>("issue_type_enum");
+        npgsqlOptions.MapEnum<AnnotationType>("annotation_type_enum");
+        npgsqlOptions.MapEnum<AssetStatus>("asset_status_enum");
     }
 }
