7ea02de3b83d41a12d19b40dc4ee65762594c68b
Cemonix <gittlermilan@seznam.cz>
2025-06-12 19:11:30 +0200

    feat: add filter parameter to GetAllAsync method in GenericRepository

--- server/Server/Repositories/Interfaces/IGenericRepository.cs
+++ server/Server/Repositories/Interfaces/IGenericRepository.cs
@@ -7,6 +7,7 @@ public interface IGenericRepository<T> where T : class
 {
     Task<T?> GetByIdAsync(object id);
     Task<IEnumerable<T>> GetAllAsync(
+        Expression<Func<T, bool>>? filter = null,
         string? filterOn = null, string? filterQuery = null, string? sortBy = null,
         bool isAscending = true, int pageNumber = 1, int pageSize = 25
     );
