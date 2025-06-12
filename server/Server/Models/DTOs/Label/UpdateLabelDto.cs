49feb5a2b3565958e93495a9424875fb6dd8be7a
Cemonix <gittlermilan@seznam.cz>
2025-06-12 17:18:24 +0200

    feat: add DTOs for label creation, update, and retrieval

---
+++ server/Server/Models/DTOs/Label/UpdateLabelDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Models.DTOs.Label
+{
+    public record class UpdateLabelDto
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
+        public string Name { get; init; } = string.Empty;
+
+        public string? Description { get; init; }
+
+        [StringLength(7)]
+        public string? Color { get; init; }
+    }
+}
