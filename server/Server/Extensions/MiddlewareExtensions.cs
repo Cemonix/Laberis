eb18ba68ee06ddab37b534744d289ad5c5ff409e
Cemonix <gittlermilan@seznam.cz>
2025-06-17 10:22:10 +0200

    feat: add UseGlobalExceptionHandling extension method for middleware configuration

---
+++ server/Server/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,19 @@
+using server.Middleware;
+
+namespace server.Extensions;
+
+/// <summary>
+/// Extension methods for configuring middleware
+/// </summary>
+public static class MiddlewareExtensions
+{
+    /// <summary>
+    /// Adds global exception handling middleware to the application pipeline
+    /// </summary>
+    /// <param name="app">The application builder</param>
+    /// <returns>The application builder for method chaining</returns>
+    public static IApplicationBuilder UseGlobalExceptionHandling(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+}
