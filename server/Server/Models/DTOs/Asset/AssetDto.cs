d939b134c66d71588693cb166f4d13fbb0170905
Cemonix <gittlermilan@seznam.cz>
2025-06-19 16:31:30 +0200

    refactor: remove unused usings from MinioStorageService and clean up AssetDto properties

--- server/Server/Models/DTOs/Asset/AssetDto.cs
+++ server/Server/Models/DTOs/Asset/AssetDto.cs
@@ -5,6 +5,8 @@ namespace server.Models.DTOs.Asset;
 public record class AssetDto
 {
     public int Id { get; init; }
+    public int ProjectId { get; init; }
+    public int DataSourceId { get; init; }
     public string ExternalId { get; init; } = string.Empty;
     public string? Filename { get; init; }
     public string? MimeType { get; init; }
@@ -15,6 +17,4 @@ public record class AssetDto
     public AssetStatus Status { get; init; }
     public DateTime CreatedAt { get; init; }
     public DateTime UpdatedAt { get; init; }
-    public int ProjectId { get; init; }
-    public int DataSourceId { get; init; }
 }
