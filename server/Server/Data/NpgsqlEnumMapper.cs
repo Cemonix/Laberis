7bd8d217b0f7cfedc7658da4e5771bffbba05e9b
Cemonix <gittlermilan@seznam.cz>
2025-06-20 17:19:05 +0200

    fix: fix enum mapping syntax to use scheme properly

--- server/Server/Data/NpgsqlEnumMapper.cs
+++ server/Server/Data/NpgsqlEnumMapper.cs
@@ -12,16 +12,16 @@ public static class NpgsqlEnumMapper
     /// <param name="npgsqlOptions">The Npgsql options builder provided by UseNpgsql().</param>
     public static void ConfigureEnums(NpgsqlDbContextOptionsBuilder npgsqlOptions)
     {
-        npgsqlOptions.MapEnum<ProjectType>("public.project_type_enum");
-        npgsqlOptions.MapEnum<ProjectStatus>("public.project_status_enum");
-        npgsqlOptions.MapEnum<ProjectRole>("public.project_role_enum");
-        npgsqlOptions.MapEnum<DataSourceType>("public.data_source_type_enum");
-        npgsqlOptions.MapEnum<DataSourceStatus>("public.data_source_status_enum");
-        npgsqlOptions.MapEnum<WorkflowStageType>("public.workflow_stage_type_enum");
-        npgsqlOptions.MapEnum<TaskEventType>("public.task_event_type_enum");
-        npgsqlOptions.MapEnum<IssueStatus>("public.issue_status_enum");
-        npgsqlOptions.MapEnum<IssueType>("public.issue_type_enum");
-        npgsqlOptions.MapEnum<AnnotationType>("public.annotation_type_enum");
-        npgsqlOptions.MapEnum<AssetStatus>("public.asset_status_enum");
+        npgsqlOptions.MapEnum<ProjectType>("project_type_enum", "public");
+        npgsqlOptions.MapEnum<ProjectStatus>("project_status_enum", "public");
+        npgsqlOptions.MapEnum<ProjectRole>("project_role_enum", "public");
+        npgsqlOptions.MapEnum<DataSourceType>("data_source_type_enum", "public");
+        npgsqlOptions.MapEnum<DataSourceStatus>("data_source_status_enum", "public");
+        npgsqlOptions.MapEnum<WorkflowStageType>("workflow_stage_type_enum", "public");
+        npgsqlOptions.MapEnum<TaskEventType>("task_event_type_enum", "public");
+        npgsqlOptions.MapEnum<IssueStatus>("issue_status_enum", "public");
+        npgsqlOptions.MapEnum<IssueType>("issue_type_enum", "public");
+        npgsqlOptions.MapEnum<AnnotationType>("annotation_type_enum", "public");
+        npgsqlOptions.MapEnum<AssetStatus>("asset_status_enum", "public");
     }
 }
