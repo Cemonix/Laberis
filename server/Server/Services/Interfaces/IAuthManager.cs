6ea53b56b2a1dbc7b8f31807db0ec95b3feb8d1c
Cemonix <gittlermilan@seznam.cz>
2025-06-22 16:36:10 +0200

    feat: define IAuthManager interface with methods for user registration, login, and token generation

--- server/Server/Services/Interfaces/IAuthManager.cs
+++ server/Server/Services/Interfaces/IAuthManager.cs
@@ -1,8 +1,11 @@
-using System;
+using Microsoft.AspNetCore.Identity;
+using server.Models.DTOs.Auth;
 
 namespace server.Services;
 
 public interface IAuthManager
 {
-
+    Task<AuthResponseDto> RegisterAsync(RegisterDto registerDto);
+    Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+    Task<string> GenerateTokenAsync(ApplicationUser user);
 }
