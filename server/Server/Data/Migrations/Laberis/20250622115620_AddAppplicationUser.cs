e1db0cad6e3fa5e6c9d51e0a9b4a0a6377f7f444
Cemonix <gittlermilan@seznam.cz>
2025-06-22 15:31:25 +0200

    Add ApplicationUserWorkflowStage table and update LaberisDbContext model snapshot
    - Created a new migration to add the ApplicationUserWorkflowStage table, establishing a many-to-many relationship between ApplicationUser and WorkflowStage.
    - Updated the LaberisDbContext model snapshot to reflect the new table and its relationships.
    - Refactored existing IdentityUser references to ApplicationUser in the model snapshot for consistency.

---
+++ server/Server/Data/Migrations/Laberis/20250622115620_AddAppplicationUser.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class AddAppplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserWorkflowStage",
+                columns: table => new
+                {
+                    AssignedUsersId = table.Column<string>(type: "text", nullable: false),
+                    WorkflowStagesWorkflowStageId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserWorkflowStage", x => new { x.AssignedUsersId, x.WorkflowStagesWorkflowStageId });
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserWorkflowStage_AspNetUsers_AssignedUsersId",
+                        column: x => x.AssignedUsersId,
+                        principalSchema: "identity",
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserWorkflowStage_workflow_stages_WorkflowStages~",
+                        column: x => x.WorkflowStagesWorkflowStageId,
+                        principalTable: "workflow_stages",
+                        principalColumn: "workflow_stage_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserWorkflowStage_WorkflowStagesWorkflowStageId",
+                table: "ApplicationUserWorkflowStage",
+                column: "WorkflowStagesWorkflowStageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserWorkflowStage");
+        }
+    }
+}
