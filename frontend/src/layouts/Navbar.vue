ff390ec50ffc4794b6ab2fdd8fa57de26365b29e
Cemonix <gittlermilan@seznam.cz>
2025-06-22 15:34:41 +0200

    feat: implement authentication pages with glass morphism design; add login and registration forms with validation; remove old registration page

--- frontend/src/layouts/Navbar.vue
+++ frontend/src/layouts/Navbar.vue
@@ -8,6 +8,11 @@
             <router-link to="/projects">Projects</router-link>
             <router-link to="/workspace/project/1/asset/1">Workspace</router-link> <!-- TODO: Hardcoded link for dev -->
         </div>
+        <div class="navbar-auth">
+            <!-- TODO: Add authentication logic -->
+            <router-link to="/login" class="auth-link">Login</router-link>
+            <router-link to="/register" class="auth-link btn-outline">Register</router-link>
+        </div>
     </nav>
 </template>
 
@@ -71,6 +76,34 @@ useRouter();
             &.router-link-exact-active::after {
                 width: 100%;
                 left: 0;
+            }
+        }
+    }
+
+    .navbar-auth {
+        display: flex;
+        gap: vars.$gap-medium;
+        align-items: center;
+
+        .auth-link {
+            color: navbar.$navbar-text;
+            text-decoration: none;
+            padding: vars.$padding-small vars.$padding-medium;
+            border-radius: vars.$border-radius-sm;
+            transition: all 0.2s ease-in-out;
+
+            &:hover {
+                color: vars.$color-link-hover;
+            }
+
+            &.btn-outline {
+                border: 1px solid vars.$color-primary;
+                color: vars.$color-primary;
+
+                &:hover {
+                    background-color: vars.$color-primary;
+                    color: vars.$color-white;
+                }
             }
         }
     }
