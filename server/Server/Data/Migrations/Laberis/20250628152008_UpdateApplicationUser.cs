a7afafbaca021027b20198faa5d126cbb1dabe3b
Cemonix <gittlermilan@seznam.cz>
2025-06-28 18:21:24 +0200

    Add RefreshToken and RefreshTokenExpiryTime to ApplicationUser
    - Created a new migration to add RefreshToken and RefreshTokenExpiryTime columns to the AspNetUsers table in the identity schema.
    - Updated the LaberisDbContextModelSnapshot to reflect the new properties in the ApplicationUser entity.

---
+++ server/Server/Data/Migrations/Laberis/20250628152008_UpdateApplicationUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class UpdateApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryTime",
+                schema: "identity",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                schema: "identity",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryTime",
+                schema: "identity",
+                table: "AspNetUsers");
+        }
+    }
+}
