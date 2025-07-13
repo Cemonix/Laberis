3435882b627cbb58dcd276422a1c538e636affbe
Cemonix <gittlermilan@seznam.cz>
2025-07-13 16:48:33 +0200

    feat: Revamp Project Dashboard with enhanced loading and error states, improved layout, and new chart components for better data visualization

--- frontend/src/views/project/ProjectDetailView.vue
+++ frontend/src/views/project/ProjectDetailView.vue
@@ -88,9 +88,8 @@ onMounted(() => {
     display: flex;
     flex-direction: column;
     flex-grow: 1;
-    padding: vars.$padding-large;
+    padding: vars.$padding-xlarge;
     width: 100%;
-    max-width: vars.$max-width-wide;
     margin: 0 auto;
 }
 
