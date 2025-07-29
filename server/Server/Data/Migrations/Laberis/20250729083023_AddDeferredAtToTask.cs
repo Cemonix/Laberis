52f6129ad2890aa622b1fc7867b98f845d5794ae
Cemonix <gittlermilan@seznam.cz>
2025-07-29 13:17:39 +0200

    Add DeferredAt column to tasks and modify details column type in task_events
    - Introduced a new nullable DateTime column named DeferredAt to the tasks table.
    - Changed the type of the details column in the task_events table from jsonb to text.

---
+++ server/Server/Data/Migrations/Laberis/20250729083023_AddDeferredAtToTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class AddDeferredAtToTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeferredAt",
+                table: "tasks",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "details",
+                table: "task_events",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "jsonb",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeferredAt",
+                table: "tasks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "details",
+                table: "task_events",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
