1e52910d0a4fccd9dca57c6b9be9d3b873adb009
Cemonix <gittlermilan@seznam.cz>
2025-08-09 11:46:00 +0200

    feat: add TaskAssigneeCell component for displaying task assignee status

---
+++ frontend/src/components/project/task/TaskAssigneeCell.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-if="assignedTo" class="assigned-user">
+        <font-awesome-icon :icon="faUser" class="user-icon" />
+        {{ assignedTo }}
+    </div>
+    <div v-else class="unassigned">
+        <font-awesome-icon :icon="faUserSlash" class="unassigned-icon" />
+        Unassigned
+    </div>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faUser, faUserSlash } from '@fortawesome/free-solid-svg-icons';
+
+interface Props {
+    assignedTo?: string;
+}
+
+defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+.assigned-user {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    
+    .user-icon {
+        color: var(--color-success);
+    }
+}
+
+.unassigned {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    color: var(--color-gray-600);
+    font-style: italic;
+    
+    .unassigned-icon {
+        color: var(--color-warning);
+    }
+}
+</style>
