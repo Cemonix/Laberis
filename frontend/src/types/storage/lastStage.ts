f0c4b6f4222251a86457544fcd4dbf885aa6974a
Cemonix <gittlermilan@seznam.cz>
2025-08-11 12:01:15 +0200

    refactor: remove refreshToken from AuthTokens and AuthResponseDto; update storage types to include LastStageData

---
+++ frontend/src/types/storage/lastStage.ts
@@ -0,0 +1,8 @@
+export interface LastStageData {
+    projectId: number;
+    workflowId: number;
+    stageId: number;
+    stageName: string;
+    projectName: string;
+    lastAccessedAt: number;
+}
