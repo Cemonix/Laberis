3be5c62756c186e2aae0a3cb9565917d81c92527
Cemonix <gittlermilan@seznam.cz>
2025-08-17 12:31:37 +0200

    Add columns for changes required and vetoed timestamps in tasks table
    - Introduced nullable DateTime columns 'changes_required_at' and 'vetoed_at' to the 'tasks' table.
    - Updated the LaberisDbContextModelSnapshot to reflect the new columns.

---
+++ server/Server/Data/Migrations/Laberis/20250817085305_AddVetoedAndChangesRequiredColumns.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class AddVetoedAndChangesRequiredColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "changes_required_at",
+                table: "tasks",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "vetoed_at",
+                table: "tasks",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "changes_required_at",
+                table: "tasks");
+
+            migrationBuilder.DropColumn(
+                name: "vetoed_at",
+                table: "tasks");
+        }
+    }
+}
