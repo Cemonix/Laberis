1745d3fa37e9b319eb088e7c49d827022ac84fd3
Cemonix <gittlermilan@seznam.cz>
2025-08-18 20:37:02 +0200

    Add Status column to tasks table in database
    - Created a new migration to add a "Status" column of type integer to the "tasks" table.
    - Updated the LaberisDbContextModelSnapshot to reflect the new "Status" property.

---
+++ server/Server/Data/Migrations/Laberis/20250818171241_AddStatusColumnToTask.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class AddStatusColumnToTask : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "tasks",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "tasks");
+        }
+    }
+}
