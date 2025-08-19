2ef19d211fe06b9e1d40ecbeb15461c5c740638b
Cemonix <gittlermilan@seznam.cz>
2025-08-19 18:42:15 +0200

    feat(migrations): update workflow stage schema
    - Removed the 'ui_configuration' column from 'workflow_stages' table.
    - Changed 'stage_type' column to be non-nullable in 'workflow_stages' table.
    - Updated LaberisDbContextModelSnapshot to reflect the changes in the 'workflow_stages' table.

---
+++ server/Server/Data/Migrations/Laberis/20250819163938_UpdateWorkflowStage.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using server.Models.Domain.Enums;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class UpdateWorkflowStage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ui_configuration",
+                table: "workflow_stages");
+
+            migrationBuilder.AlterColumn<WorkflowStageType>(
+                name: "stage_type",
+                table: "workflow_stages",
+                type: "public.workflow_stage_type_enum",
+                nullable: false,
+                oldClrType: typeof(WorkflowStageType),
+                oldType: "public.workflow_stage_type_enum",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<WorkflowStageType>(
+                name: "stage_type",
+                table: "workflow_stages",
+                type: "public.workflow_stage_type_enum",
+                nullable: true,
+                oldClrType: typeof(WorkflowStageType),
+                oldType: "public.workflow_stage_type_enum");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ui_configuration",
+                table: "workflow_stages",
+                type: "jsonb",
+                nullable: true);
+        }
+    }
+}
