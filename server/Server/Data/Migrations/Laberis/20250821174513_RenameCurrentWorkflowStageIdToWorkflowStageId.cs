909bd8df1c628f980edadb750f9bffc594a9ac65
Cemonix <gittlermilan@seznam.cz>
2025-08-21 19:53:11 +0200

    Rename 'current_workflow_stage_id' to 'workflow_stage_id' in tasks table and update related foreign key constraints and model snapshot.

---
+++ server/Server/Data/Migrations/Laberis/20250821174513_RenameCurrentWorkflowStageIdToWorkflowStageId.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Data.Migrations.Laberis
+{
+    /// <inheritdoc />
+    public partial class RenameCurrentWorkflowStageIdToWorkflowStageId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_workflow_stages_current_workflow_stage_id",
+                table: "tasks");
+
+            migrationBuilder.RenameColumn(
+                name: "current_workflow_stage_id",
+                table: "tasks",
+                newName: "workflow_stage_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_tasks_current_workflow_stage_id",
+                table: "tasks",
+                newName: "IX_tasks_workflow_stage_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_workflow_stages_workflow_stage_id",
+                table: "tasks",
+                column: "workflow_stage_id",
+                principalTable: "workflow_stages",
+                principalColumn: "workflow_stage_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_workflow_stages_workflow_stage_id",
+                table: "tasks");
+
+            migrationBuilder.RenameColumn(
+                name: "workflow_stage_id",
+                table: "tasks",
+                newName: "current_workflow_stage_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_tasks_workflow_stage_id",
+                table: "tasks",
+                newName: "IX_tasks_current_workflow_stage_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_workflow_stages_current_workflow_stage_id",
+                table: "tasks",
+                column: "current_workflow_stage_id",
+                principalTable: "workflow_stages",
+                principalColumn: "workflow_stage_id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
